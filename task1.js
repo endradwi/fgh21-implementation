a4747b34ef7b60e65d77922f5c8fae0aa6e867b3
endradwi <94545255+endradwi@users.noreply.github.com>
2024-06-10 15:54:40 +0700

    Add files via upload

--- task1.js
+++ task1.js
@@ -1,39 +1,10 @@
-function deteksiPalindrom(kata){
-const upperToLower = {
-    'A': 'a', 'B': 'b', 'C': 'c', 'D': 'd', 'E': 'e', 'F': 'f', 'G': 'g',
-    'H': 'h', 'I': 'i', 'J': 'j', 'K': 'k', 'L': 'l', 'M': 'm', 'N': 'n',
-    'O': 'o', 'P': 'p', 'Q': 'q', 'R': 'r', 'S': 's', 'T': 't', 'U': 'u',
-    'V': 'v', 'W': 'w', 'X': 'x', 'Y': 'y', 'Z': 'z'
-}
-let update = ''
-for (let i = 0; i < kata.length; i++) {
-    let huruf = kata[i]
-    if (huruf >= 'A') {
-        if (huruf <= 'Z') {
-            update += upperToLower[huruf]
-        }
+function cekPalindrom(kata) {
+    let hasil = ""
+    for (let i = kata.length; i > 0; i--) {
+        hasil = hasil + kata[i - 1]
     }
-    if (huruf >= 'a') {
-        if (huruf <= 'z') {
-            update += huruf
-        }
+    if (hasil === kata) {
+        console.log("Palindrom")
     }
 }
-let balikKata = ''
-for (let i = update.length - 1; i >= 0; i--) {
-    balikKata += update[i]
-}
-let deteksiPalindrom = true
-for (let i = 0; i < update.length; i++) {
-    if (update[i] !== balikKata[i]) {
-        deteksiPalindrom = false
-    }
-}
-return deteksiPalindrom
-}
-let kata = "Malam";
-if (deteksiPalindrom(kata)) {
-    console.log("Palindrom");
-} else {
-    console.log();
-}
+cekPalindrom("katak")
