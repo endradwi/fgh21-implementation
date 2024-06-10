b40f5b3ff8cbe37c9ad9e4b031c62ef050d6c3db
endradwi <94545255+endradwi@users.noreply.github.com>
2024-06-10 17:18:50 +0700

    Add files via upload

--- task3.js
+++ task3.js
@@ -1,29 +1,50 @@
 function FazzFood(harga, voucher, jarak, pajak) {
-    let potongan = 0
-    if (voucher === 'FAZZFOOD50') {
-        if (harga >= 50000) {
-            potongan = harga * 0.5
-        }
-    } if (voucher === 'DITRAKTIR60') {
-        if (harga >= 25000) {
-            potongan = harga * 0.6
-        }
+    const availableVoucer = {
+        FAZZFOOD50 : {
+            min : 50000,
+            diskon : 50 / 100,
+            max : 50000
+        },
+        DIRAKTIR60 : {
+            min : 25000,
+            diskon : 60 / 100,
+            max : 30000
+        } 
+    }
+   
+    if(!availableVoucer[voucher]){
+        console.log("Voucher is not available!")
+        return
     }
 
+    voucher = availableVoucer[voucher]
+    if(harga < voucher.min){
+        console.log("Voucher min order is invalid")
+        return
+    }
+
+    let disc = harga * voucher.diskon
+    if (disc > voucher.max){
+        disc = voucher.max
+    }
+    
     let biayaAntar = 5000
     if (jarak > 2) {
         biayaAntar += (jarak - 2) * 3000
     }
-    let biayaPajak = 0;
-    if (pajak) {
-        biayaPajak = harga * 0.05;
-    }
-    let subtotal = harga - potongan + biayaAntar + biayaPajak;
 
-    console.log("Harga : " + harga);
-    console.log("Potongan : " + potongan);
-    console.log("Biaya Antar : " + biayaAntar);
-    console.log("Pajak : " + biayaPajak);
-    console.log("Subtotal : " + subtotal);
+    if (pajak === true) {
+        pajak = harga * 5 / 100
+    }else{
+        tax = 0
+    }
+
+    let subtotal = harga - disc + biayaAntar + pajak
+   
+    console.log("Harga : " + harga)
+    console.log("Potongan : " + disc)
+    console.log("Biaya Antar : " + biayaAntar)
+    console.log("Pajak : " + pajak)
+    console.log("Subtotal : " + subtotal)
 }
-FazzFood(75000, 'FAZZFOOD50', 5, true);
+FazzFood(75000,'FAZZFOOD50', 5, true)
